c09c25b99717310e63ac67196126972ae330cf26
John Desrosiers <johndrunner@yahoo.com>
2017-12-11 15:10:09 -0800

    initial html and form adds new task to list

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # TO DO List - Firebase
-A To Do list application built with Firebase. Create, update, and check off tasks.
+A To Do list application built with Firebase. Create, update, and check off tasks. List is shared by all users of the application.
 
 ## How to Use the App
 Use the form to add tasks. Click on link to edit. Click on check mark to marke the task complete, removing it from the list.
 
-Your list will be there next time you visit the site.
+## Future Development Ideas
+* Choose a list to view instead of only one list for all users to interact with.
+* Only authorized users may view a list.
+* A list can have administrator/owner/user
+
+The list is updated in real time as all users interact with the list.
 
 ---
 Created by John Desrosiers. December 2017.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,25 @@
-console.log('hello todo list');
+// when user clicks add
+// get input from the form
+// add task to list
+// clear form input
+
+// Function to create new htlm element for a task and append it to the task list
+function appendNewTask(text) {
+  $('.list-group').append(
+    '<li class="list-group-item"><div class="row"><div class="col-xs-9">' +
+    '<span class="glyphicon glyphicon-unchecked"></span>' +
+    $('<span>').text(' ' + text).html() +
+    '</div><div class="col-xs-3"><span class="glyphicon glyphicon-edit pull-right">' +
+    '</span></div></div></li>');
+}
+function addTask(event) {
+  event.preventDefault();
+  var $newTaskInput = $('#txtNewTask');
+  var newTask = $newTaskInput.val().trim();
+  $newTaskInput.val('');
+  appendNewTask(newTask);
+}
+
+$(function onDocumentReady() {
+  $('form').on('submit', addTask);
+});
