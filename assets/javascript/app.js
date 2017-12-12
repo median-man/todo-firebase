1526d8236ec8219d5b52e1b5a2565ab597f15e83
John Desrosiers <johndrunner@yahoo.com>
2017-12-12 06:16:19 -0800

    task updating works. modified task removal.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,15 +4,25 @@ var tasksRef = database.ref('/tasks');
 
 var task = {
   create: function createNewTask(text) {
-    var key = tasksRef.push(text).key;
+    var newTaskRef = tasksRef.push(text);
     return {
-      key: key, // keep things es5 compatible
+      key: newTaskRef.key, // keep things es5 compatible
       text: text
     };
   },
   // method deletes task from database and returns promise
   delete: function deleteTask(key) {
     return tasksRef.child(key).remove();
+  },
+  // method updates task from the database and returns promise
+  // passing an object with the key and new text
+  update: function updateTask(key, text) {
+    return tasksRef
+      .child(key)
+      .set(text)
+      .then(function () {
+        return { key: key, text: text };
+      });
   }
 };
 
@@ -22,15 +32,19 @@ function appendNewTask(newTask) {
     class: 'glyphicon glyphicon-unchecked',
     data: newTask
   });
-  var $textSpan = $('<span>').text(' ' + newTask.text);
+  // give the span holding the text of the task an id equal to the key
+  var $textSpan = $('<span>').attr('id', newTask.key).text(' ' + newTask.text);
   var $edit = $('<span>').addClass('glyphicon glyphicon-edit pull-right').data(newTask);
   var $col1 = $('<div>').addClass('col-xs-9').append($checkBox, $textSpan);
   var $col2 = $('<div>').addClass('col-xs-3').append($edit);
   var $row = $('<div>').addClass('row').append($col1, $col2);
-  return $('<li class="list-group-item"></li>')
-    .attr('id', newTask.key)
-    .append($row)
-    .appendTo('.list-group');
+  return $('<li class="list-group-item"></li>').append($row).appendTo('.list-group');
+}
+
+// Function to update the text of a task in the todo list by id
+function renderTask(taskData) {
+  console.log('rendering task', taskData);
+  $('#' + taskData.key).text(taskData.text);
 }
 
 // Function adds user input from add task form and clears the form.
@@ -48,34 +62,36 @@ function handleTaskCheckClick() {
   var key = $(this).data('key');
   task
     .delete(key)
-
-    // remove task from list once task has been removed from database
     .then(function deleteTaskElement() {
-      $('#' + key).remove();
+      // remove the task from the page
+      $('#' + key).parents('li').remove();
     });
 }
 
 function handleEditTaskFormSubmit(event) {
   event.preventDefault();
-  console.log($('#txtUpdateTask').val());
+  var $input = $('#txtUpdateTask');
+  var key = $input.data().key;
+  var text = $input.val().trim();
+
+  // update the task if the text was changed
+  if (text !== $input.data().text) task.update(key, text);
+
+  // hide the modal
+  $('#editTaskModal').modal('hide');
 }
 
 // Function to handle click on an edit element for a task
 function handleEditTaskClick() {
   // populate form with task data
   var task = $(this).data();
-  $('#txtUpdateTask').val(task.text);
+  $('#txtUpdateTask').val(task.text).data(task);
 
   // display a form to edit the task
   $('#editTaskModal').modal();
   // when user clicks ok
 
   $('#editTaskForm').data(task).on('submit', handleEditTaskFormSubmit);
-
-
-  // update the task and close the form
-  // when user clicks cancel
-  // close the form without updating the task
 }
 
 // set event listeners
@@ -87,10 +103,11 @@ $(function onDocumentReady() {
   // append task to page when added to database
   tasksRef.on('child_added', function handleTaskAdded(childSnap) {
     appendNewTask({ key: childSnap.key, text: childSnap.val() });
+
+    // render any changes to the task data
+    childSnap.ref.on('value', function (snap) {
+      // update the displayed text if snap wasn't deleted
+      if (snap.val()) $('#' + snap.key).text(' ' + snap.val());
+    });
   });
 });
-
-// when check mark is click
-// get the key of the clicked task
-// remove the task from the database for the key
-// remove task from list once task has been removed
