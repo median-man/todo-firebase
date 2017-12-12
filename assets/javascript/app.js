13e07bf289ecdb5f9bb819e35c369efa64010221
John Desrosiers <johndrunner@yahoo.com>
2017-12-11 17:45:19 -0800

    read and create tasks from database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,15 @@
-// when user clicks add
-// get input from the form
-// add task to list
-// clear form input
+// database globals
+var database = firebase.database();
+
+var task = {
+  newTask: function createNewTask(text) {
+    var key = database.ref().push(text).key;
+    return {
+      key: key, // keep things es5 compatible
+      text: text
+    };
+  }
+};
 
 // Function to create new htlm element for a task and append it to the task list
 function appendNewTask(text) {
@@ -12,14 +20,35 @@ function appendNewTask(text) {
     '</div><div class="col-xs-3"><span class="glyphicon glyphicon-edit pull-right">' +
     '</span></div></div></li>');
 }
-function addTask(event) {
+
+// Function adds user input from add task form and clears the form.
+function handleAddTaskFormSubmit(event) {
   event.preventDefault();
   var $newTaskInput = $('#txtNewTask');
   var newTask = $newTaskInput.val().trim();
   $newTaskInput.val('');
-  appendNewTask(newTask);
+  console.log(task.newTask(newTask));
 }
 
+// Function to handle click on an edit element for a task
+function handleEditTaskClick() {
+  // when the user clicks on an edit button
+  // get the id for the task that was clicked
+  // display a form to edit the task
+  // when user clicks ok
+  // update the task and close the form
+  // when user clicks cancel
+  // close the form without updating the task
+  console.log(this);
+}
+
+// set event listeners
+$(document).on('click', '.glyphicon-edit', handleEditTaskClick);
 $(function onDocumentReady() {
-  $('form').on('submit', addTask);
+  $('form').on('submit', handleAddTaskFormSubmit);
+
+  // append task to page when added to database
+  database.ref().on('child_added', function handleTaskAdded(childSnap) {
+    appendNewTask(childSnap.val());
+  });
 });
