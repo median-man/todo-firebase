2c979a35093016dc935ecc08d647ea106910d837
John Desrosiers <johndrunner@yahoo.com>
2017-12-16 06:07:02 -0800

    display log in modal and validate input

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -1,15 +1,46 @@
+// Function to validate the log in form data. Returns an object containing
+// the result and failure messages.
+function validateLogInForm(email, pass) {
+  var result = { isValid: true };
+
+  // password must be 8 or more characters
+  if (pass.length < 8) {
+    result.isValid = false;
+    result.password = 'Password must have at least 8 characters.';
+  }
+
+  // email must not be an empty string
+  if (email.length < 1) {
+    result.isValid = false;
+    result.email = 'Please provide a valid email address.';
+  }
+
+  // must be a valid email (contains only valid characters and required characters)
+  return result;
+}
+
 function initLogIn() {
-  // show log in modal
-  var $logInModal = $('#logInModal').modal();
+  var $logInModal = $('#logInModal');
+  var $emailInput = $('#email');
+  var $passInput = $('#password');
+  // show log in modal and give the email field the focus
+  $logInModal.on('shown.bs.modal', function () {
+    $emailInput.focus();
+  });  
+  $logInModal.modal();
 
   // set event listeners for UI events
   $('#logInModal form').on('submit', function (event) {
     event.preventDefault();
-    var formData = {
-      userName: $('#email').val().trim(),
-      password: $('#password').val().trim()
-    };
-    console.log('log in submitted', formData);
+    var email = $emailInput.val().trim();
+    var password = $passInput.val().trim();
+    var formValidation = validateLogInForm(email, password);
+    if (formValidation.isValid) {
+      console.log('form data is valid. email:', email, ', password:', password);
+    } else {
+      console.log('invalid form data. email:', email, ', password:', password, formValidation);
+    }
+    
     $logInModal.modal('hide');
   });
 }
