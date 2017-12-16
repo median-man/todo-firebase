d81a8eacc7d28f77c319c42f97d81068c5c90425
John Desrosiers <johndrunner@yahoo.com>
2017-12-16 06:36:25 -0800

    validateLogInForm gets and returns form data

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -1,47 +1,70 @@
+// Function to create a new user account. Returns promise containing firebase.User object.
+function createNewUser(email, password) {
+  console.log('creating new user. email: ' + email + ', password: ' + password);
+  return firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(error);
+    // ...
+  });
+}
+
 // Function to validate the log in form data. Returns an object containing
 // the result and failure messages.
-function validateLogInForm(email, pass) {
-  var result = { isValid: true };
+function validateLogInForm() {  
+  var $emailInput = $('#email');
+  var $passInput = $('#password');
+  var email = $emailInput.val().trim();
+  var pass = $passInput.val().trim();
+  var result = {
+    isValid: true,
+    password: pass,
+    email: email,
+    error: false
+  };
 
   // password must be 8 or more characters
   if (pass.length < 8) {
     result.isValid = false;
-    result.password = 'Password must have at least 8 characters.';
+    result.error.password = 'Password must have at least 8 characters.';
   }
 
   // email must not be an empty string
   if (email.length < 1) {
     result.isValid = false;
-    result.email = 'Please provide a valid email address.';
+    result.error.email = 'Please provide a valid email address.';
   }
-
-  // must be a valid email (contains only valid characters and required characters)
   return result;
 }
 
 function initLogIn() {
   var $logInModal = $('#logInModal');
   var $emailInput = $('#email');
-  var $passInput = $('#password');
+
   // show log in modal and give the email field the focus
   $logInModal.on('shown.bs.modal', function () {
     $emailInput.focus();
   });  
   $logInModal.modal();
 
-  // set event listeners for UI events
-  $('#logInModal form').on('submit', function (event) {
+  // user log in
+  $logInModal.find('form').on('submit', function (event) {
     event.preventDefault();
-    var email = $emailInput.val().trim();
-    var password = $passInput.val().trim();
-    var formValidation = validateLogInForm(email, password);
-    if (formValidation.isValid) {
-      console.log('form data is valid. email:', email, ', password:', password);
+    var formData = validateLogInForm();
+    if (formData.isValid) {
+      console.log('form data is valid.', formData);
     } else {
-      console.log('invalid form data. email:', email, ', password:', password, formValidation);
+      console.log('invalid form data.', formData);
     }
     
     $logInModal.modal('hide');
+  });
+
+  // user sign
+  $('#signUp').on('click', function () {
+    var formData = validateLogInForm();
+    console.log('sign up clicked', formData);
   });
 }
 $(document).ready(initLogIn);
